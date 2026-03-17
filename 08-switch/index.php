756a112048b0677144e04c9855558d68ef988d82
Pedro Baltar <pedrobaltarduarte@gmail.com>
2026-03-17 14:36:10 -0300

    adiciona 08-switch

---
+++ 08-switch/index.php
@@ -0,0 +1,25 @@
+<?php
+
+
+$grade = "Pizza";
+
+switch($grade) {
+    case "A":
+        echo "You did great";
+        break;
+    case "B":
+        echo "You did good";
+        break;
+    case "C":
+        echo "You did ok";
+        break;
+    case "D":
+        echo "You did poorly";
+        break;
+    case "F":
+        echo "You failed";
+        break;
+}
+
+
+?>
