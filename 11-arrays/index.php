91fee9becbd2a941feb328f6fec1d3d13d61d05f
Pedro Baltar <pedrobaltarduarte@gmail.com>
2026-03-17 22:35:10 -0300

    arrays

---
+++ 11-arrays/index.php
@@ -0,0 +1,17 @@
+<?php 
+
+// array = "variavel" que pode armazenar mais de um valor de uma vez
+
+    $foods = array("apple", "orange", "banana", "coconut");
+
+    //$foods[0] = "pineapple";
+    //array_push($foods, "pineapple", "kiwi");
+    //array_pop($foods);
+    //array_shift($foods);
+    //$foods = array_reverse($foods);
+    echo count($foods);
+    
+    /* foreach($foods as $food) {
+        echo $food . "<br>"; 
+    } */
+?>
